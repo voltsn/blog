c086f86a1c41e58503c3d985ff21398ae48adc7c
Arno Volts <voltsnntanisimine@gmail.com>
2025-03-09 05:32:24 +0100

    Add tanstack router

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,19 @@
+import { createRootRoute, Link, Outlet} from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+        <Link to="/test" className="[&.active]:font-bold">
+         Test 
+        </Link>
+      </div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  )
+});
